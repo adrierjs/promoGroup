323fcfce5087f882f08149e0145d5e9c773d97a9
adrier.santos <adrier.santos@newtail.com.br>
2023-08-27 00:56:42 -0300

    Refatoração - For

--- index.js
+++ index.js
@@ -9,13 +9,12 @@ axios.get(url)
     const $ = cheerio.load(html);
     const productNamePrice = $('.ProductCard_ProductCard_Name__LT7hv, p.Text_Text__h_AF6.Text_MobileHeadingS__Zxam2');
 
-    for (let index = 0; index < productNamePrice.length; index++) {
-        const element = productNamePrice[index];
-        const elementText = $(element).text()
-        if(elementText != 'Filtros'){
-            console.log(elementText)
-            }
+    productNamePrice.each(function(index, element){
+      const elementText = $(element).text()
+      if(elementText != 'Filtros'){
+        console.log(elementText)
         }
+       });
     })
   .catch((error) => {
     console.log(error);
