f2752b4086eee08ba927b0aadfe98fc61e852bc8
adrier.santos <adrier.santos@newtail.com.br>
2023-09-07 19:08:25 -0300

    add - script em python

---
+++ integrationSite.py
@@ -0,0 +1,25 @@
+import undetected_chromedriver as uc
+import time
+
+class IntegrationWithApi:
+    def __init__(self, site):
+        self._site = site
+        self._options = uc.ChromeOptions()
+
+    def websiteConnection(self):
+        try:
+            self._options.headless = True
+            _driver = uc.Chrome(use_subprocess=True, options=self._options)
+            _driver.get(self._site)
+            _driver.maximize_window()
+            # time.sleep(50)
+            _driver.save_screenshot("validation.png")
+            _dataSite = _driver.page_source
+        except Exception as erro:
+            raise f'Erro {erro}'
+        finally:
+            _driver.close()
+        return _dataSite
+
+integration = IntegrationWithApi("https://www.zoom.com.br/celular")
+dataSite = integration.websiteConnection()
---
+++ zoomIntegration/dataProcess.py
@@ -0,0 +1,60 @@
+from bs4 import BeautifulSoup
+from integrationSite import dataSite
+import json
+class DataConverter:
+    def __init__(self,dataSite):
+        self._dataSite = dataSite
+        self.__soup = BeautifulSoup(self._dataSite, 'html.parser')
+        self.__elements_with_class = self.__soup.find_all(class_="ProductCard_ProductCard_Inner__tsD4M")
+
+
+    def imagem(self):
+            for elements in self.__elements_with_class:
+                img_element = elements.find("img")
+                if img_element:
+                    src_value = img_element.get("src")
+                    print(src_value)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    def namePriceOffer(self):
+        list_products = []
+
+        for elements in self.__elements_with_class:
+            href = elements.get("href")
+            product_name = elements.find('h2', {'data-testid': 'product-card::name'}).text.strip()
+            product_price = elements.find('p',{'data-testid':'product-card::price'}).text.strip()
+            if href and "/celular" in href:
+                full_link = f"https://www.zoom.com.br{href}"
+                product = {"name": product_name,
+                           "price":product_price,
+                           "product_link": full_link}
+
+                list_products.append(product)
+            else:
+                product = {"name": product_name,
+                           "price": product_price,
+                           "product_link": href}
+                list_products.append(product)
+        return list_products
+        # for product in list_products:
+        #     print(product)
+
+
+
+
+
+dataConverter = DataConverter(dataSite)
+products = dataConverter.imagem()
+# print(products)
