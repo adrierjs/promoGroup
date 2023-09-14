24c1dd3326bf57a631475fa54b4c87cf666052e8
Ronildo Lima <ronildolima996@gmail.com>
2023-09-14 17:14:10 -0300

    Tela inicial

---
+++ app_promo/views.py
@@ -0,0 +1,40 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+
+def cadastro(request):
+    if request.method == 'POST':
+        username = request.POST.get('email')
+        email = request.POST.get('email')
+        first_name = request.POST.get('nome')
+        password = request.POST.get('password')
+
+        result = User.objects.filter(username=username).first()
+        if result:
+            messages.error(request, "Este nome de usuário já está em uso. Tente outro.")
+        else:
+            user = User.objects.create_user(username=username,
+                                            email=email,
+                                            first_name=first_name,
+                                            password=password)
+            user.save()
+            login(request, user)
+            redirect('/')
+    return render(request, "cadastro.html")
+
+def Login(request):
+    if request.method == 'POST':
+        username = request.POST.get('email')
+        password = request.POST.get('password')
+
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            login(request, user)
+            redirect('/')
+        else:
+            messages.error(request, "Seu usuário ou senha estão incorretos.")
+    return render(request,'login.html')
+
+def index(request):
+    return render(request, "index.html")
---
+++ project_promo/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path
+from app_promo import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('cadastro/', views.cadastro, name='cadastro'),
+    path('login/', views.Login, name='login'),
+    path('', views.index, name='index')
+]
