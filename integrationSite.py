0bc975c089d04977d9a59402ba7a9c8a4927f5a1
Adrier José <80015771+adrierjs@users.noreply.github.com>
2023-09-29 11:58:40 -0300

    Revert "Feature/sp 64"

--- integrationSite.py
+++ integrationSite.py
@@ -1,4 +1,6 @@
 import requests
+
+
 class IntegrationWithWebSite:
     def __init__(self, site):
         self._site = site
@@ -7,7 +9,11 @@ class IntegrationWithWebSite:
         try:
             response = requests.get(self._site)
             response.raise_for_status()
-            return response.text, response.status_code
+            return response.text
 
         except requests.exceptions.RequestException as error:
-            raise f'Erro na requisição: {error}'
+            raise f'Erro {error}'
+
+
+integration = IntegrationWithWebSite("https://www.zoom.com.br/celular")
+dataSite = integration.websiteConnection()
