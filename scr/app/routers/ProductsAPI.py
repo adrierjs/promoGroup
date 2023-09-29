88ae07ba2278edcef54d9f3e7c143d3e6faed983
adrier.santos <adrier.santos@newtail.com.br>
2023-09-29 11:58:02 -0300

    implementando api rest

---
+++ scr/app/routers/ProductsAPI.py
@@ -0,0 +1,36 @@
+from flask import Flask, jsonify
+from scr.integrationSite import IntegrationWithWebSite
+from scr.integration_zoom.dataProcess import DataConverter
+
+app = Flask(__name__)
+
+@app.route('/api/celular', methods=['GET'])
+def get_exemplo():
+    integration = IntegrationWithWebSite("https://www.zoom.com.br/celular")
+    dataSite, statusCode = integration.websiteConnection()
+    dataConverter = DataConverter(dataSite)
+    retorno = (dataConverter.namePriceOffer())
+
+    return jsonify(retorno)
+
+@app.route('/api/tv', methods=['GET'])
+def getNotebook():
+    integration = IntegrationWithWebSite("https://www.zoom.com.br/tv")
+    dataSite, statusCode = integration.websiteConnection()
+    dataConverter = DataConverter(dataSite)
+    retorno = (dataConverter.namePriceOffer())
+
+    return jsonify(retorno)
+
+@app.route('/api/ar-condicionado', methods=['GET'])
+def getArcondicionado():
+    integration = IntegrationWithWebSite("https://www.zoom.com.br/ar-condicionado")
+    dataSite, statusCode = integration.websiteConnection()
+    dataConverter = DataConverter(dataSite)
+    retorno = (dataConverter.namePriceOffer())
+
+    return jsonify(retorno)
+if __name__ == '__main__':
+    # Por padrão, o Flask executará na porta 5000
+    app.run(host='127.0.0.1', port=8080)
+
