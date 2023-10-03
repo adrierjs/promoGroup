3b2fafaa8a23eb2d248293a6156da960e8d77c90
Adrier José <80015771+adrierjs@users.noreply.github.com>
2023-10-03 00:08:00 -0300

    Update settings.py

--- project_promo/settings.py
+++ project_promo/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = 'django-insecure-2kdvb+jmo49#6x9@i(=tvgow$x#$i4cmtr(msm=kbkihbd-+-q
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
