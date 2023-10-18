5fcd411b077c216b83bdcd124d6aea1836e6aeb2
adrier.santos <adrier.santos@newtail.com.br>
2023-10-18 01:01:10 -0300

    implementando logs na api

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 import os
 from flask import Flask
+import logging
 from src.app.routers.routes import product_routes
 
 app = Flask(__name__)
 
 app.register_blueprint(product_routes)
+log_filename = 'app_logs.log'
+logging.basicConfig(filename=log_filename, level=logging.INFO, format='%(asctime)s [%(levelname)s] - %(message)s')
 
 if __name__ == '__main__':
     os.environ['FLASK_ENV'] = 'development'
