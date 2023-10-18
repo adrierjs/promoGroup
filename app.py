15d56a30d5a088b5da3efb420012c1a7f05c7add
adrier.santos <adrier.santos@newtail.com.br>
2023-10-18 15:32:46 -0300

    corrigindo bug do cors

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 import os
 from flask import Flask
 import logging
+from flask_cors import CORS  # Importe a extensão Flask-CORS
 from src.app.routers.routes import product_routes
 
 app = Flask(__name__)
+
+# Configure o CORS
+CORS(app, resources={r"/api/*": {"origins": "*"}})
 
 app.register_blueprint(product_routes)
 log_filename = 'app_logs.log'
@@ -12,3 +16,4 @@ logging.basicConfig(filename=log_filename, level=logging.INFO, format='%(asctime
 if __name__ == '__main__':
     os.environ['FLASK_ENV'] = 'development'
     app.run(host='0.0.0.0', port=5000)
+
