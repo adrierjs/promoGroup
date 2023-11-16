be6ad13924ef01f5aea64dc018a59d15156c9f0d
adrierjs <adrierj@gmail.com>
2023-11-16 00:53:47 -0300

    fix - api

--- static/js/innerGeladeira.js
+++ static/js/innerGeladeira.js
@@ -1,40 +1,42 @@
 const telefonesInfo = [];
 
+fetch("http://54.89.213.137:5000/geladeira")
+  .then((res) => res.json())
+  .then((data) => {
+    console.log("Dados da API:", data);
 
-fetch('http://52.87.204.54:5000/geladeira')
-  .then(res => res.json())
-  .then(data => {
-    console.log('Dados da API:', data);
-
-    data.forEach(product => {
-      const link = product.productLink.replace('http://127.0.0.1:8000/api/', '');
+    data.forEach((product) => {
+      const link = product.productLink.replace(
+        "http://127.0.0.1:8000/api/",
+        ""
+      );
 
       telefonesInfo.push({
         nome: product.name,
         preco: product.price,
         link: link,
-        image: product.srcImg
+        image: product.srcImg,
       });
     });
 
-    console.log(telefonesInfo)
+    console.log(telefonesInfo);
 
     const jsonTelefones = JSON.stringify(telefonesInfo, null, 2);
     console.log(jsonTelefones);
 
-    const ul = document.getElementById('listaSmart');
+    const ul = document.getElementById("listaSmart");
     showGallery(telefonesInfo, ul);
   })
-  .catch(error => console.error('Ocorreu um erro:', error));
+  .catch((error) => console.error("Ocorreu um erro:", error));
 
-  function showGallery(telefonesInfo, ul) {
-    ul.innerHTML = '';
-  
-    telefonesInfo.forEach((item, index) => {
-      const li = document.createElement("li");
-      li.classList.add('col-lg-3', 'col-md-4', 'col-sm-6', 'pb-1');
-      const link = `http://${item.link}`;
-      li.innerHTML = `
+function showGallery(telefonesInfo, ul) {
+  ul.innerHTML = "";
+
+  telefonesInfo.forEach((item, index) => {
+    const li = document.createElement("li");
+    li.classList.add("col-lg-3", "col-md-4", "col-sm-6", "pb-1");
+    const link = `http://${item.link}`;
+    li.innerHTML = `
       <div class="product-item bg-light mb-4">
       <div class="product-img position-relative overflow-hidden">
           <img class="img-fluid w-100" src="${item.image}" alt="">
@@ -50,28 +52,26 @@ fetch('http://52.87.204.54:5000/geladeira')
       </div>
   </div>
           `;
-      ul.appendChild(li);
-    });
-  }
-
-
+    ul.appendChild(li);
+  });
+}
 
 // Adicione um evento de escuta ao campo de entrada
-const input = document.getElementById('product-input');
-input.addEventListener('input', filtrarLista);
+const input = document.getElementById("product-input");
+input.addEventListener("input", filtrarLista);
 
 function filtrarLista() {
-    const ul = document.getElementById('listaSmart');
-    const textoFiltrado = input.value.toLowerCase(); // Obtenha o texto inserido e converta para minúsculas para comparação
+  const ul = document.getElementById("listaSmart");
+  const textoFiltrado = input.value.toLowerCase(); // Obtenha o texto inserido e converta para minúsculas para comparação
 
-    // Iterar sobre as <li> e verificar se o texto filtrado está contido no nome do telefone
-    const liItens = ul.querySelectorAll('li');
-    liItens.forEach((li) => {
-        const nomeItem = li.querySelector('.h6').textContent.toLowerCase();
-        if (nomeItem.includes(textoFiltrado)) {
-            li.style.display = 'block'; // Mostrar a <li> se corresponder ao filtro
-        } else {
-            li.style.display = 'none'; // Ocultar a <li> se não corresponder ao filtro
-        }
-    });
+  // Iterar sobre as <li> e verificar se o texto filtrado está contido no nome do telefone
+  const liItens = ul.querySelectorAll("li");
+  liItens.forEach((li) => {
+    const nomeItem = li.querySelector(".h6").textContent.toLowerCase();
+    if (nomeItem.includes(textoFiltrado)) {
+      li.style.display = "block"; // Mostrar a <li> se corresponder ao filtro
+    } else {
+      li.style.display = "none"; // Ocultar a <li> se não corresponder ao filtro
+    }
+  });
 }
